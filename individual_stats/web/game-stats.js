b558f9eaf3659680e5da36d673cdacc31b875982
alexpcapp <alexander.cappelen@hotmail.com>
2025-11-06 17:44:05 +0100

    no changes

--- individual_stats/web/game-stats.js
+++ individual_stats/web/game-stats.js
@@ -138,6 +138,7 @@ function populateSummaryTable(matchId) {
     { key: "Digs", label: "Most Digs" },
   ];
 
+  
   categories.forEach(cat => {
     let bestPlayer = null;
     let bestValue = -Infinity;
